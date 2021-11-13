7d9501ae0e324094ea07550ea104914abcffb50d
Kaan Yenel <79691374+KaanYenel@users.noreply.github.com>
2021-11-14 01:11:01 +0300

    Add files via upload

---
+++ Chapter_01/Exercise_01_01/Exercise_01_01.java
@@ -0,0 +1,8 @@
+
+public class Exercise_01_01 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}

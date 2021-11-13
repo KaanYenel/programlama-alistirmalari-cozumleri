2134d61844920c992555da59280083ae47ce35cf
Kaan Yenel <79691374+KaanYenel@users.noreply.github.com>
2021-11-14 01:19:12 +0300

    Add files via upload

---
+++ Chapter_01/Exercise_01_02/Exercise_01_02.java
@@ -0,0 +1,10 @@
+
+public class Exercise_01_02 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}

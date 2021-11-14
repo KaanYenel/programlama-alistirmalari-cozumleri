da249d303f5a0c0f9c11559a994088ee57b30ca4
Kaan Yenel <79691374+KaanYenel@users.noreply.github.com>
2021-11-15 01:44:25 +0300

    Add files via upload

---
+++ Chapter_01/Exercise_01_08/Exercise_01_08.java
@@ -0,0 +1,7 @@
+
+public class Exercise_01_08 {
+    public static void main(String[] args){
+        System.out.println("Dairenin çevresi = " + (2 * 5.5 * 3.14159));
+        System.out.println("Dairenin alanı = " + (5.5 * 5.5 * 3.14159));
+    }
+}

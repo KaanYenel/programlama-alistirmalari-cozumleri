3fbdddc30599068d1b29f70c5cf7180a62e600c0
Kaan Yenel <79691374+KaanYenel@users.noreply.github.com>
2021-11-15 01:45:05 +0300

    Add files via upload

---
+++ Chapter_01/Exercise_01_09/Exercise_01_09.java
@@ -0,0 +1,7 @@
+
+public class Exercise_01_09 {
+    public static void main(String[] args){
+        System.out.println("Dikdörtgenin çevresi = " + (2 * 4.5 + 2 * 7.9));
+        System.out.println("Dikdörtgenin alanı = " + (4.5 * 7.9));
+    }
+}
